26339d4355920630cdd69be8a2aa80c38450bb20
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-02-20 21:47:23 +0600

    Button Toggling

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("JS Connected");
+let thrivingList = [];
+let strugglingList = [];
+
+thrivingList.push({ name: "plant 1" });
+
+
+let total = document.getElementById("total");
+let thrivingCount = document.getElementById("thrivingCount");
+let strugllingCount = document.getElementById("strugglingCount");
+
+
+const allCardSection = document.getElementById("allCards");
+
+const mainContainer = document.querySelector("main");
+
+
+function calculateCount() {
+  total.innerText = allCardSection.children.length;
+  thrivingCount.innerText = thrivingList.length;
+  strugllingCount.innerText = strugglingList.length;
+}
+
+calculateCount();
+
+const allFilterBtn = document.getElementById("all-filter-btn");
+
+
+function toggleStyle(id){
+    console.log("Clicked")
+}
